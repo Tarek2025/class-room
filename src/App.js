aa0376a04d6c67fa81b008e4b47e0f60d97835ca
Tarek2025 <tarek2022hasan@gmail.com>
2020-08-28 01:44:57 +0600

    amader School

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Course from './Components/Course/Course';
+import EnrolledCourse from './Components/EnrolledCourse/EnrolledCourse';
+import fakeData from './fakeData/fakeData.js';
+import { useState } from 'react';
+import Header from './Components/Header/Header';
 
 function App() {
+  // shuffle the array. 
+  for (let i = 0; i < fakeData.length; i++) {
+    const randomNumber = Math.floor(Math.random() * Math.floor(fakeData.length));
+    const temp = fakeData[randomNumber];
+    fakeData[randomNumber] = fakeData[i];
+    fakeData[i] = temp;
+  }
+
+  const [ enroll, setEnroll] = useState([]);
+
+  const enrollCourse = (newCourse) => {
+    const newEnroll = [...enroll, newCourse];
+    setEnroll(newEnroll);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <div className="container courses">
+        <h4 className="text-center">Get started with these courses</h4>
+        {
+          fakeData.map(data => <Course enrollCourse={enrollCourse} key={data.id} data={data}></Course>)
+        }
+      </div>
+      <div className="clear"></div>
+      <div className="container enrolled">
+        <EnrolledCourse enroll={enroll}></EnrolledCourse>
+      </div>
     </div>
   );
 }
